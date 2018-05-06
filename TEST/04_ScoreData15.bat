b724c9531c8111cdf801a2966a165ffe3321eca1
vzhomeexperiments <vz.home.experiments@gmail.com>
2018-05-06 16:58:56 +0200

    creating bat files

---
+++ TEST/04_ScoreData15.bat
@@ -0,0 +1,5 @@
+@echo off
+:: Use this line of code for debugging purposes *** Adapt the paths for R installation and R script!!!
+::"C:\Program Files\R\R-3.4.3\bin\R.exe" CMD BATCH "C:\Users\fxtrams\Documents\000_TradingRepo\R_selflearning\04_ScoreDataM15.R"
+:: Use this code in 'production'
+"C:\Program Files\R\R-3.4.3\bin\Rscript.exe" "C:\Users\fxtrams\Documents\000_TradingRepo\R_selflearning\04_ScoreDataM15.R
