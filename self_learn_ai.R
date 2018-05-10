b6d858f83c09309f8e041b2d4b791fa75e9a14fb
vzhomeexperiments <vz.home.experiments@gmail.com>
2018-05-10 11:35:52 +0200

    using function for self learning

--- self_learn_ai.R
+++ self_learn_ai.R
@@ -1,11 +1,16 @@
 ## FUNCTION self_learn_ai
-## PURPOSE: function gets price data, indicator data, number of columns as input
-## it is splitting this data by number or inputs, transpose this data
+## PURPOSE: function gets price data, indicator data, number of predictors as input
+## it is splitting this data by number or rows, transpose this data
 ## additionally it is label the data based on the simple logic assigning it to 2 categories based on the difference
 ## between beginning and end of the vector
-## finally it is stacking all data and joining everything into the table 
+## finally it is stacking all data and joining everything into the table
+## see more detailed info inside Udemy course Lazy Trading Part 7: Developing Self Learning Robot
 
-self_learn_ai <- function(price_dataset, indicator_dataset, num_bars){
+## NB: functions below must be available inside the R Environment!
+# source("C:/Users/fxtrams/Documents/000_TradingRepo/R_selflearning/create_labelled_data.R")
+# source("C:/Users/fxtrams/Documents/000_TradingRepo/R_selflearning/create_transposed_data.R")
+
+self_learn_ai <- function(price_dataset, indicator_dataset, num_bars, timeframe){
 
 ### use commented code below to test this function  
  # source("C:/Users/fxtrams/Documents/000_TradingRepo/R_selflearning/create_labelled_data.R")
@@ -13,6 +18,7 @@ self_learn_ai <- function(price_dataset, indicator_dataset, num_bars){
  # price_dataset <- read_rds("test_data/prices.rds")
  # indicator_dataset <- read_rds("test_data/macd.rds")
  # num_bars <- 100
+ # timeframe <- 1 # indicates the timeframe used for training (e.g. 1 minute, 15 minutes, 60 minutes, etc)
   
 # transform data and get the labels shift rows down
 dat14 <- create_labelled_data(price_dataset, num_bars) %>% mutate_all(funs(lag), n=28) 
@@ -33,8 +39,8 @@ macd_ML  <- as.h2o(x = dat16, destination_frame = "macd_ML")
   
 # fit models from simplest to more complex
 ModelC <- h2o.deeplearning(
-  model_id = "DL_Classification1",
-  x = names(macd_ML[,2:101]), 
+  model_id = paste0("DL_Classification", timeframe),
+  x = names(macd_ML[,2:num_bars+1]), 
   y = "LABEL",
   training_frame = macd_ML,
   activation = "Tanh",
@@ -49,30 +55,19 @@ ModelC <- h2o.deeplearning(
   balance_classes = F,
   epochs = 200)
 
-
-}
-
-
-
-
-
-
 #ModelC
 #summary(ModelC)
 #h2o.performance(ModelC)
 
 ## Checking how the model predict using the latest dataset
 # get the labelled data for the test
-dat17 <- create_labelled_data(prices, num_bars) %>% select(LABEL) %>% head(28)
+dat17 <- create_labelled_data(price_dataset, num_bars) %>% select(LABEL) %>% head(28)
 # transform data for indicator and get the subset to predict
-dat18 <- create_transposed_data(macd, num_bars) %>% head(56) %>% tail(28) %>% 
+dat18 <- create_transposed_data(indicator_dataset, num_bars) %>% head(56) %>% tail(28) %>% 
   # need to add fake category to avoid h2o prediction function errors
   mutate(LABEL = "BU") %<>% 
   # same as data_latest$LABEL <- as.factor(data_latest$LABEL)
-  mutate_at(101, as.factor) 
-
-
-
+  mutate_at(num_bars+1, as.factor) 
 
 # upload recent dataset to predict
 recent_ML  <- as.h2o(x = dat18, destination_frame = "recent_ML")
@@ -83,12 +78,25 @@ result <- h2o.predict(ModelC, recent_ML) %>% as.data.frame() %>% select(predict)
   # count matches
   summarise(Quality = sum(MATCH)/28)
 
-# save the model in case it's correctly predicting in more than 90% of the cases
+# save the model in case it's correctly predicting in more than 90% of the cases for the very last observation
 if(result$Quality > 0.9){
   h2o.saveModel(ModelC, path = "model/", force = T)
 }
 
 h2o.shutdown(prompt = FALSE)
+
+
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
