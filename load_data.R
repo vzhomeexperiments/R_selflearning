d536dc2edb4dfdfecc07badf7afd8f7e432d6d43
vzhomeexperiments <vz.home.experiments@gmail.com>
2018-07-08 16:00:08 +0200

    updated load data function to account for different file size

--- load_data.R
+++ load_data.R
@@ -6,11 +6,13 @@
 #' Load Data Function
 #' https://www.udemy.com/self-learning-trading-robot/?couponCode=LAZYTRADE7-10
 #' 
-#' Function imports file and change date column type. Function return the dataframe with trade data
+#' Function imports file and change date column type. Function return the dataframe with trade data. 
+#' -> update: data_deepth argument was added to optimize code and separate data needed to train and score the model
 #'
 #' @param path_terminal - path to the MT4 terminal, string
 #' @param trade_log_file - csv file name where the data is stored, without ".csv"
 #' @param time_period - data periodicity in minutes, can be 1, 15, 60
+#' @param data_deepth - collected data deepth in rows. describe how many rows in original file to read
 #'
 #' @return - dataframe with asset data in columns where X1 column is in a POSIXct format
 #' @export
@@ -18,17 +20,19 @@
 #' @examples  
 #' prices <- load_data(path_terminal = "C:/Program Files (x86)/FxPro - Terminal2/MQL4/Files/",
 #'                     trade_log_file = "AI_CP", 
-#'                     time_period = 1)
+#'                     time_period = 1,
+#'                     data_deepth = 14200)
 #' 
-load_data <- function(path_terminal, trade_log_file, time_period = 1){
+load_data <- function(path_terminal, trade_log_file, time_period = 1, data_deepth = 14200){
   # path_terminal <- "C:/Program Files (x86)/FxPro - Terminal2/MQL4/Files/"
   # path_terminal <- file.path(getwd(), "test_data")
   # trade_log_file <- "AI_CP"
   # trade_log_file <- "AI_Macd"
   # time_period <- 60
+  # data_deepth <- 14200
   require(tidyverse)
   require(lubridate)
-  DFT1 <- try(read_csv(file = file.path(path_terminal, paste0(trade_log_file, time_period, ".csv")),
+  DFT1 <- try(read_csv(file = file.path(path_terminal, paste0(trade_log_file, time_period, "-", data_deepth, ".csv")),
                        col_names = F),
               silent = TRUE)
   if(class(DFT1)[1] == "try-error") {stop("Error reading file. File with trades may not exist yet!",
