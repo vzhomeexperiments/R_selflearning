933ae08e1155ca2d56e1fd794ce24cf72322863b
vzhomeexperiments <vz.home.experiments@gmail.com>
2020-09-24 20:00:46 +0200

    delete duplicate script

--- logs_analyse/analyse_results1.R
+++ logs_analyse/analyse_results1.R
@@ -1,6 +1,7 @@
 # 20200913 - Analysing Model Testing results
 
 library(ggplot2)
+library(magrittr)
 
 ## read files with predicted price changes
 # path
@@ -24,3 +25,25 @@ ggplot(dfres, aes(x = NB_hold, y = PnL_NB,
               #size = TotalTrades, 
               col = as.factor(Symbol)))+geom_point()
 
+
+## Analysis of model quality records
+# file names
+filesToAnalyse1 <-list.files(path = path_logs,
+                            pattern = "StrTest-",
+                            full.names=TRUE)
+
+# aggregate all files into one
+for (VAR in filesToAnalyse1) {
+  # VAR <- filesToAnalyse1[1]
+  if(!exists("dfres1")){dfres1 <- readr::read_csv(VAR)}  else {
+    dfres1 <- readr::read_csv(VAR) %>% dplyr::bind_rows(dfres1)
+  }
+  
+}
+
+# visualized
+ggplot(dfres1, aes(x = MaxPerf, y = Symbol,
+                  col = TR_Level, 
+                  size = NB_hold))+geom_point()+
+                  geom_vline(xintercept=0.001)+ 
+                  scale_x_continuous(trans='log10')
