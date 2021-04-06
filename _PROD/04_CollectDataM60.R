790d155b0c24f7c3ae4610c2e1775adf31cf8cd5
vzhomeexperiments <vz.home.experiments@gmail.com>
2021-04-06 21:17:24 +0200

    re-organized code using env. variables

--- _PROD/04_CollectDataM60.R
+++ _PROD/04_CollectDataM60.R
@@ -1,7 +1,7 @@
 # ----------------------------------------------------------------------------------------
 # R Script to collect (aggregate) the asset indicator data and respective prices
 # ----------------------------------------------------------------------------------------
-# (C) 2020 Vladimir Zhbanko
+# (C) 2020, 2021 Vladimir Zhbanko
 # https://www.udemy.com/course/self-learning-trading-robot/?referralCode=B95FC127BA32DA5298F4
 #
 # load libraries to use and custom functions
@@ -13,6 +13,8 @@
 
 
 #### Read asset prices and indicators ==========================================
+## pre-requisite - deploy DataWriter Robot on Terminal 2
+# https://github.com/vzhomeexperiments/DataWriter/blob/master/DataWriter_v6.01.mq4
 #absolute path with the data (choose MT4 directory where files are generated)
 #!!!Setup Environmental Variables!!! 
 path_terminal <- normalizePath(Sys.getenv('PATH_T2'), winslash = '/')
@@ -24,6 +26,8 @@ path_user <- file.path(path_user, "R_selflearning")
 
 #path with the data
 path_data <- file.path(path_user, "_DATA")
+#create directory if not exists
+if(!dir.exists(path_data)){dir.create(path_data)}
 
 # Vector of currency pairs used
 Pairs = c("EURUSD", "GBPUSD", "AUDUSD", "NZDUSD", "USDCAD", "USDCHF", "USDJPY",
@@ -39,10 +43,11 @@ ind = file.path(path_terminal, paste0("AI_RSIADX",PAIR,"60",".csv")) %>% read_cs
 ind$X1 <- ymd_hms(ind$X1)  
   
 # data transformation using the custom function for one symbol
-aml_collect_data(indicator_dataset = ind,
-                 symbol = PAIR,
-                 timeframe = 60,
-                 path_data = path_data)
+lazytrade::aml_collect_data(indicator_dataset = ind,
+                            symbol = PAIR,
+                            timeframe = 60,
+                            path_data = path_data,
+                            max_nrows = 2500)
   
  #full_path <- file.path(path_data, 'AI_RSIADXEURUSD60.rds')
    
--- _PROD1/06_TestBuildTestModel60.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-:: Use this code in 'production'
-%PATH_REXE% "%PATH_DSS_Repo%\R_selflearning\_PROD1\06_TestBuildTestModelM60.R"
